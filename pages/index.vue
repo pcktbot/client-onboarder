84669ccbebb0f495d8ea0aeb1ca334dcfcaccd0c
David Miller <david.miller@getg5.com>
2020-11-19 11:30:20 -0800

    1.0.9

--- pages/index.vue
+++ pages/index.vue
@@ -13,7 +13,7 @@
             />
             <b-input-group-append>
               <b-btn variant="outline-secondary">
-                Switch
+                Switch Project
               </b-btn>
             </b-input-group-append>
           </b-input-group>
@@ -23,15 +23,22 @@
       <section class="main-content py-2">
         <project-details />
         <b-card no-body header-class="border-0" class="border-0">
-          <b-tabs card>
-            <b-tab title="Start" title-link-class="py-4 px-3 text-uppercase text-muted font-weight-bold">
+          <b-tabs v-model="currentTab" card>
+            <b-tab title="1. Welcome" title-link-class="p-4 text-uppercase text-muted font-weight-bold">
+              <welcome-start />
+            </b-tab>
+            <b-tab title="2. Start" title-link-class="p-4 text-uppercase text-muted font-weight-bold">
               <location-start />
             </b-tab>
-            <b-tab title="General" title-link-class="py-4 px-3 text-uppercase text-muted font-weight-bold">
-              <accordion-wrapper v-bind="{ categories }" />
+            <b-tab title="3. General" title-link-class="p-4 text-uppercase text-muted font-weight-bold">
+              <accordion-wrapper v-bind="{ categories, prefix: 'general' }" />
             </b-tab>
-            <b-tab title="Tab" lazy title-link-class="py-4 px-3 text-uppercase text-muted font-weight-bold" />
-            <b-tab title="Tab (Disabled)" lazy disabled title-link-class="py-4 px-3 text-uppercase text-muted font-italic" />
+            <b-tab title="4. Bulk Edit" lazy title-link-class="p-4 text-uppercase text-muted font-weight-bold" class="p-0">
+              <bulk-edit-wrapper>
+                <accordion-wrapper v-bind="{ categories: bulk, prefix: 'bulk' }" />
+              </bulk-edit-wrapper>
+            </b-tab>
+            <b-tab title="5. Tab (Disabled)" lazy disabled title-link-class="p-4 text-uppercase text-muted font-italic" />
           </b-tabs>
         </b-card>
       </section>
@@ -43,6 +50,7 @@
 export default {
   data () {
     return {
+      currentTab: 0,
       userHasMultipleProjects: true,
       categories: [
         {
@@ -72,20 +80,22 @@ export default {
               id: 'domain-registar',
               placeholder: 'Example: GoDaddy'
             }],
-            [{
-              type: 'text',
-              label: 'Domain Manager Name',
-              group: 'domain-manager',
-              id: 'domain-manager-name',
-              placeholder: ''
-            }],
-            [{
-              type: 'email',
-              label: 'Domain Manager Email',
-              group: 'domain-manager',
-              id: 'domain-manager-email',
-              placeholder: 'Example: user@domain.com'
-            }]
+            [
+              {
+                type: 'text',
+                label: 'Domain Manager Name',
+                group: 'domain-manager',
+                id: 'domain-manager-name',
+                placeholder: ''
+              },
+              {
+                type: 'email',
+                label: 'Domain Manager Email',
+                group: 'domain-manager',
+                id: 'domain-manager-email',
+                placeholder: 'Example: user@domain.com'
+              }
+            ]
           ],
           fieldData: {
             'current-website': null,
@@ -158,9 +168,65 @@ export default {
             websiteLeadEmail: null,
             officeHours: null
           }
+        },
+        {
+          label: 'Property Type',
+          id: 'property-type',
+          description: '',
+          time: '45 mins',
+          isComplete: false,
+          isBulk: true,
+          isCorp: false,
+          fields: [
+            [{
+              label: 'Geographic Type',
+              id: 'geographic',
+              type: 'select',
+              options: ['Suburban', 'Urban']
+            }],
+            [{
+              label: 'Class',
+              id: 'class',
+              type: 'select',
+              options: ['Class A', 'Class B', 'Class C']
+            }],
+            [{
+              label: 'Target Lifestyle',
+              id: 'targetLifestyle',
+              type: 'select',
+              options: ['On the Go', 'Happy at Home']
+            }],
+            [{
+              label: 'Feature',
+              id: 'feature',
+              type: 'select',
+              options: ['Luxury', 'Affordable', 'Modern', 'Gated', 'Furnished']
+            }],
+            [{
+              label: 'Status',
+              id: 'status',
+              type: 'select',
+              options: ['High Occupancy', 'Mid Occupancy', 'Low Occupancy', 'Lease Up', 'Development']
+            }],
+            [{
+              label: 'Is Your Location specifical design for Senior or Student demographics?',
+              id: 'demographicTarget',
+              type: 'select',
+              options: ['Senior', '55+', 'Student', 'N/A']
+            }]
+          ],
+          fieldData: {
+            geographic: null,
+            class: null,
+            targetLifestyle: null,
+            feature: null,
+            status: null,
+            demographicTarget: 'N/A'
+          }
         }
       ],
       selected: [],
+      bulk: [],
       locations: [
         {
           name: 'The Junction',
@@ -177,12 +243,6 @@ export default {
         { name: 'Location-7', status: 'Incomplete', data: [] },
         { name: 'Location-3', status: 'Incomplete', data: [] },
         { name: 'Location-3', status: 'Incomplete', data: [] },
-        { name: 'Location-6', status: 'Incomplete', data: [] },
-        { name: 'Location-3', status: 'Incomplete', data: [] },
-        { name: 'Location-3', status: 'Incomplete', data: [] },
-        { name: 'Location-3', status: 'Incomplete', data: [] },
-        { name: 'Location-3', status: 'Incomplete', data: [] },
-        { name: 'Location-3', status: 'Incomplete', data: [] },
         { name: 'Location-3', status: 'Incomplete', data: [] },
         { name: 'Location-3', status: 'Incomplete', data: [] },
         { name: 'Location-3', status: 'Incomplete', data: [] },
@@ -191,6 +251,9 @@ export default {
         { name: 'Location-4', status: 'Incomplete', data: [] }
       ]
     }
+  },
+  created () {
+    this.bulk = this.categories.filter(cat => cat.isBulk)
   }
 }
 </script>
