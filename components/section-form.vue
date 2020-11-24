041fb5047b69ccc50188ce0c57874e7b898b7e61
David Miller <david.miller@getg5.com>
2020-11-24 08:43:09 -0800

    custom select

--- components/section-form.vue
+++ components/section-form.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="px-5 py-3">
+  <div class="px-5 py-5">
     <div
       v-for="(field, i) in fields"
       :key="`field-row-${i}`"
-      class="d-flex justify-content-between w-100"
+      class="d-flex justify-content-between w-100 mb-0 flex-wrap"
     >
       <b-form-group
         v-for="f in field"
         :key="f.id"
         :class="{ 'inline-group-ctn': f.hasMergeConflict }"
+        :style="{ 'max-width: 50%;': field.length > 1 }"
         label-class="text-uppercase text-muted font-weight-bold"
         class="mr-2"
-        :style="fields.length === 2 ? 'flex: 1 0 50%' : 'flex: 1 0 auto'"
+        style="flex: 1 1 auto;"
       >
         <!-- form field label starts -->
         <template v-slot:label>
@@ -24,27 +25,38 @@
           />
         </template>
         <!-- form field label ends -->
-        <b-form-select
+        <span
           v-if="f.type === 'select'"
-          :value="fieldData[f.id]"
-          :options="f.options"
-          :disabled="f.hasMergeConflict"
-        />
+          class="section-group"
+        >
+          <b-form-select
+            :value="fieldData[f.id]"
+            :options="f.options"
+            :disabled="f.hasMergeConflict"
+            class="section-group__select"
+          />
+          <span class="section-group__caret">
+            <b-icon-chevron-down
+              variant="light"
+            />
+          </span>
+        </span>
         <b-form-input
           v-else
           :type="f.type"
           :value="fieldData[f.id]"
           :placeholder="f.placeholder"
           :disabled="f.hasMergeConflict"
+          class="section-input"
         />
         <difference-resolver v-if="f.hasMergeConflict" />
       </b-form-group>
     </div>
-    <div class="d-flex justify-content-end mb-3">
+    <div class="d-flex justify-content-end mb-3 mt-4">
       <b-btn
         variant="outline-error-20"
         class="text-uppercase font-weight-bold mr-2 px-4"
-        style="border-width: 0px; border-radius: 8px;"
+        style="border-width: 0px; border-radius: 6px;"
       >
         Abandon Changes
       </b-btn>
@@ -130,8 +142,37 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 .inline-group-ctn {
   position: relative;
+}
+.section-group {
+  position: relative;
+  &__select {
+    background: none;
+    background-color: white;
+    border: 2px solid #93999f;
+    border-radius: 6px;
+    box-shadow: inset 0 1px 4px rgba(203, 206, 209, 1);
+  }
+  &__caret {
+    pointer-events: none;
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 1.5em;
+    height: 1.5em;
+    transform: translate(-50%, 0.25em);
+    background-color: #93999f;
+    z-index: 11;
+    border-radius: 6px;
+    text-align: center;
+    box-shadow: 0 1px 4px rgba(203, 206, 209, 1);
+  }
+}
+.section-input.form-control {
+  border: 2px solid #93999f;
+  border-radius: 6px;
+  box-shadow: inset 0 1px 4px rgba(203, 206, 209, 1);
 }
 </style>
