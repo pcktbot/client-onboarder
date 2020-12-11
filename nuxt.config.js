d2ccec22f11bb94efa6186d24e0bde39ab3065b0
David Miller <david.miller@getg5.com>
2020-12-11 14:40:53 -0800

    retitle app

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
     port: process.env.PORT || 8085
   },
   head: {
-    title: 'client-onboarder',
+    title: 'Onboarding',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
