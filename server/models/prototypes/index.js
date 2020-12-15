5af46689b72904a16b58c276accade89333eae0e
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-12-14 20:45:00 -0800

    add location form

--- server/models/prototypes/index.js
+++ server/models/prototypes/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-function addPrototypes(models, sequelize, Sequelize) {
+function addPrototypes (models, sequelize, Sequelize) {
   fs.readdirSync(__dirname)
     .filter(file => file.indexOf('.') !== 0 && file !== 'index.js') // get all the model files
     .forEach((file) => {
---
+++ server/models/section.js
@@ -0,0 +1,24 @@
+const { INTEGER, JSONB } = require('sequelize')
+
+module.exports = (sequelize) => {
+  const section = sequelize.define('section', {
+    id: {
+      type: INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
+    },
+    name: {
+      type: JSONB
+    },
+    priority: {
+      type: INTEGER
+    }
+  })
+  section.associate = (models) => {
+    section.hasMany(models.field)
+    section.hasMany(models.subSection)
+    section.belongsToMany(models.package, { through: 'sectionPackage' })
+  }
+  return section
+}
